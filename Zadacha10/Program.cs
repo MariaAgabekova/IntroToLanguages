eaee6fdd8967e8829b72386f13983e23cb13c723
Maria Agabekova <m312102@bk.ru>
2022-11-19 01:06:53 +0600

    Add solution to Zadacha10

---
+++ Zadacha10/Program.cs
@@ -0,0 +1,30 @@
+﻿void Zadacha10()
+{
+    /*
+    Задача 10: Напишите программу, которая принимает на вход 
+    трёхзначное число и на выходе показывает вторую цифру этого числа.
+   456 -> 5
+   782 -> 8
+   918 -> 1
+    */
+
+Console.WriteLine("Введите трёхзначное положительное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    
+    if(number / 100 > 0 & number / 100 < 10)
+    {
+    int hundreds = number / 100;
+    int tens = number / 10;
+    int result = (number - hundreds * 100) / 10;
+    
+    Console.WriteLine($"Вторая цифра трехзначного числа равна {result}");
+    }
+
+    else
+    {
+     Console.WriteLine($"{number} не является положительным трехзначным числом");
+    }
+
+   
+}
+Zadacha10();

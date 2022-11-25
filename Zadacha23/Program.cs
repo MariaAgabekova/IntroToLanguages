6bd1b9c2bffc5a55c8f50f656adca47171e7e0be
Maria Agabekova <m312102@bk.ru>
2022-11-25 13:48:48 +0600

    Add solution to Zadacha23

---
+++ Zadacha23/Program.cs
@@ -0,0 +1,17 @@
+﻿void Zadacha23()
+{
+    //Задача 23. Напишите программу, которая принимает на вход 
+    //число (N) и выдаёт таблицу кубов чисел от 1 до N.
+    // 3 -> 1, 8, 27
+    // 5 -> 1, 8, 27, 64, 125
+    Random rand = new Random();
+    int N = rand.Next(1, 20);
+    Console.WriteLine("Введено число: " + N);
+    int result = 1;
+    for (int i = 1; i <= N; i++)
+    {
+        Console.WriteLine($"{i} ^ 3 = {i * i * i}");
+        Console.WriteLine();
+    }
+}
+Zadacha23();

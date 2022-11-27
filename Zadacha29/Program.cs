a09709f300e7f9fdf4e0ac3393b3a4810f981fd5
Maria Agabekova <m312102@bk.ru>
2022-11-27 14:21:24 +0600

    Черновое решение Zadacha29

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,43 @@
+﻿void Zadacha29()
+{
+    //Задача 29: Напишите программу, которая задаёт массив из 8 
+    //случайных целых чисел и выводит отсортированный по модулю массив.
+    // -2, 1, -7, 5, 19 -> [1, -2, 5, -7, 19]
+    // 6, 1, -33 -> [1, 6, -33]
+    int[] array = new int[8];
+    void FillArray()
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = new Random().Next(-40, 40);
+            Console.Write(array[i] + " ");
+        }
+    }
+    FillArray();
+    void selectionSort()
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            int minPosition = i;
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[j] < array[minPosition])
+                {
+                    minPosition = j;
+                }
+            }
+        }
+    }
+    selectionSort();
+    void PrintArray(int[] col)
+    {
+        int count = col.Length;
+        int position = 0;
+        while (position < count)
+        {
+            Console.WriteLine(col[position]);
+            position++;
+        }
+    }
+}
+Zadacha29();

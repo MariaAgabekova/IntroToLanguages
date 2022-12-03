fabca7bedad6c85213a85d5942e2386457f41261
Maria Agabekova <m312102@bk.ru>
2022-12-03 13:53:35 +0600

    Add solution to Zadacha34

--- Zadacha34/Program.cs
+++ Zadacha34/Program.cs
@@ -3,38 +3,27 @@
     /* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
     Напишите программу, которая покажет количество чётных чисел в массиве.
     [345, 897, 568, 234] -> 2  */
-    int size = ReadInt("Введите размер массива: ");
+    Console.Write("Введите размер массива: ");
+    int size = Convert.ToInt32(Console.ReadLine());
     int[] numbers = new int[size];
+    int count = 0;
     FillArray(numbers);
     PrintArray(numbers);
-    int result = 0;
 
     for (int i = 0; i < numbers.Length; i++)
     {
         if (numbers[i] % 2 == 0)
-        {
-            result++;
-        }
+            count++;
     }
-    if (result % 10 == 1)
-    {
-        Console.WriteLine($"В массиве {result} четное число");
-    }
-    if (result % 10 == 2 || result % 10 == 3 || result % 10 == 4)
-    {
-        Console.WriteLine($"В массиве {result} четных числа");
-    }
-    else
-        Console.WriteLine($"В массиве {result} четных чисел");
+    Console.WriteLine($"Количество чётных чисел в массиве: {count} ");
 
     void FillArray(int[] array)
     {
         for (int i = 0; i < array.Length; i++)
         {
-            array[i] = new Random().Next(99, 999);
+            array[i] = new Random().Next(100, 1000);
         }
     }
-
     void PrintArray(int[] array)
     {
         for (int i = 0; i < array.Length; i++)
@@ -42,12 +31,6 @@
             Console.Write(array[i] + " ");
         }
         Console.WriteLine();
-    }
-
-   int ReadInt(string message)
-    {
-        Console.Write(message);
-        return Convert.ToInt32(Console.ReadLine());
     }
 }
 Zadacha34();

d6b7c5cb1aef4e9a083c4e4a7f49cb73b58ac9c7
Maria Agabekova <m312102@bk.ru>
2022-12-11 15:34:28 +0600

    Add solution to Zadacha54

---
+++ Zadacha54/Program.cs
@@ -0,0 +1,63 @@
+﻿void Zadacha54()
+{
+    /*Задача 54. Задайте двумерный массив. Напишите программу, которая 
+    упорядочит по убыванию элементы каждой строки двумерного массива.
+    */
+
+    Console.Write("Введите количество строк: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите количество столбцов: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    int[,] numbers = new int[rows, columns];
+    FillArray(numbers);
+    Console.WriteLine();
+    Console.WriteLine("Массив до изменения:");
+    PrintArray(numbers);
+
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1) - 1; j++)
+        {
+            for (int z = 0; z < numbers.GetLength(1) - 1; z++)
+            {
+                if (numbers[i, z] < numbers[i, z + 1])
+                {
+                    int temp = 0;
+                    temp = numbers[i, z];
+                    numbers[i, z] = numbers[i, z + 1];
+                    numbers[i, z + 1] = temp;
+                }
+            }
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine("Массив с упорядоченными значениями: ");
+    PrintArray(numbers);
+}
+Zadacha54();
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
